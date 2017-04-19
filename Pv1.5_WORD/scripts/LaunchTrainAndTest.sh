77cd8bb721f9d04e5de0defe310071023354dd3d
francescofiorenza <cesco1902@hotmail.it>
2017-04-19 21:13:07 +0200

    Pv1.5_WORD

---
+++ Pv1.5_WORD/scripts/LaunchTrainAndTest.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+outputFolder=../output
+plotFolder=$outputFolder/PLOT
+ConceptPlotFolder=$plotFolder/CONCEPT
+
+start1=`date +%s`
+echo "############### START TRAIN and TEST SIMULATION ##################"
+echo " "
+echo "####Start simulation date `date`"
+  start2=`date +%s`
+  echo "-- START TRAINING PHASE "
+      ./runTrain.sh
+  echo "-- FINISH TRAINING PHASE IN  $((`date +%s`-start2)) sec ---"
+  start2=`date +%s`
+  echo "-- START TESTING PHASE "
+    ./runTest.sh
+  echo "-- FINISH TESTING PHASE IN  $((`date +%s`-start2)) sec "
+echo "######FINISH TRAIN and TEST SIMULATION IN $((`date +%s`-start1)) sec ######"
+echo "######FINISH simulation date `date`"
+
+echo -e "Method\tgramOrder\tTimeSec" > $ConceptPlotFolder/ClassificationTime.log
+cat outputLaunchTrainAndTest.WORDasTOKEN.log |grep "test in"|awk '$5 {OFS="\t"; print $9,$7,$5}'>>$ConceptPlotFolder/ClassificationTime.log
